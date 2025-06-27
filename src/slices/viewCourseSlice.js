f1c4ebc0ba87f68f3c1ad689a40e80cc67dc1e35
Gaurav Gupta <gaurav901705@gmail.com>
2025-06-27 11:46:31 -0700

    project

---
+++ src/slices/viewCourseSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  courseSectionData: [],   //section array
+  courseEntireData: [],    // single course data has populated instructor details with additional details, section and subsection details, rating and review details, category details 
+  completedLectures: [],    //completed subsections array 
+  totalNoOfLectures: 0,    //total sunsections of each section of this course
+}
+
+const viewCourseSlice = createSlice({
+  name: "viewCourse",
+  initialState,
+  reducers: {
+    setCourseSectionData: (state, action) => {
+      state.courseSectionData = action.payload
+    },
+    setEntireCourseData: (state, action) => {
+      state.courseEntireData = action.payload
+    },
+    setTotalNoOfLectures: (state, action) => {
+      state.totalNoOfLectures = action.payload
+    },
+    setCompletedLectures: (state, action) => {
+      state.completedLectures = action.payload
+    },
+    updateCompletedLectures: (state, action) => {
+      state.completedLectures = [...state.completedLectures, action.payload]
+    },
+  },
+})
+
+export const {
+  setCourseSectionData,
+  setEntireCourseData,
+  setTotalNoOfLectures,
+  setCompletedLectures,
+  updateCompletedLectures,
+} = viewCourseSlice.actions
+
+export default viewCourseSlice.reducer
